03731b16fee6dd7e24a12f5c7c7dcf84066ca25d
Deivid Ugarte Quesada <Deivid Ugarte Quesada>
2016-05-28 19:22:15 -0600

    init

--- MyFan/src/main/java/myfan/resources/MyResource.java
+++ MyFan/src/main/java/myfan/resources/MyResource.java
@@ -1,4 +1,4 @@
-package MyMusic.MyFan;
+package myfan.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        return "Hola Valeria";
     }
 }
