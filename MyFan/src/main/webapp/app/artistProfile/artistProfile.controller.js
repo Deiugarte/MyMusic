792b63898bab4036d0a69bc0946ddfe76084d2af
Deivid Ugarte Quesada <dugarte@pernix-solutions.com>
2016-06-16 21:12:45 -0600

    change on header

--- MyFan/src/main/webapp/app/artistProfile/artistProfile.controller.js
+++ MyFan/src/main/webapp/app/artistProfile/artistProfile.controller.js
@@ -114,11 +114,11 @@
         vm.artistCommentsList = {
             comments: [{
                     author: "Alejandro",
-                    comment: "Para mi es un objecto de culto. Su voz y su musica son una pura expresion de fe. con el te lleva casi al más alla. es uno de los padre del movimiento hippie, uno de los mas carismaticos.",
+                    body: "Para mi es un objecto de culto. Su voz y su musica son una pura expresion de fe. con el te lleva casi al más alla. es uno de los padre del movimiento hippie, uno de los mas carismaticos.",
                     stars: 3
                 }, {
                     author: "RastaGirl201",
-                    comment: "Aunque esté muerto su estilo perdurará por siempre y es el rey del regge y de África. - Ventajas: Puro Regge, Aún vive su espíritu - Desventajas: Por desgracia para todos no está presente :(",
+                    body: "Aunque esté muerto su estilo perdurará por siempre y es el rey del regge y de África. - Ventajas: Puro Regge, Aún vive su espíritu - Desventajas: Por desgracia para todos no está presente :(",
                     stars: 5
                 }
 
