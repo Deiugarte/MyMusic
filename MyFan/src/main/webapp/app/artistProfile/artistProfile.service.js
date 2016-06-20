3cd3b62bb149abadc209a8f2082ec7a70970238b
Deivid Ugarte Quesada <dugarte@pernix-solutions.com>
2016-06-19 18:32:47 -0600

    timeline for artits profile

--- MyFan/src/main/webapp/app/artistProfile/artistProfile.service.js
+++ MyFan/src/main/webapp/app/artistProfile/artistProfile.service.js
@@ -12,7 +12,7 @@
     var vm= this;
     vm.getTimelineNews = getTimelineNews;
     vm.getTimelineEvents = getTimelineEvents;
-
+    vm.getUserData=getUserData;
     function getTimelineNews(data){
       var url = 'http://localhost:8000/rest/v1/news/getRecent'
       return $http.post(url,data);
@@ -21,6 +21,11 @@
     function getTimelineEvents(data){
       var url = 'http://localhost:8000/rest/v1/events/getRecent'
       return $http.post(url,data);
+    }
+
+    function getUserData(data) {
+      var url = 'http://localhost:8000/rest/v1/resources/artistData/'+data.UserId;
+      return $http.get(url, data);
     }
   }
 })();
